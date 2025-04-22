581fae5176999757543fb861b4bc612dd0f78513
Fernando Falci <falci@falci.me>
2025-04-22 15:02:32 +0200

    feat(release): Add GitHub actions workflow for automated releases

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(false)
-        .compile_protos(&["resources/proto/lnrpc.proto"], &["resources/proto"])
+    let protoc = protoc_bin_vendored::protoc_bin_path().expect("Failed to find vendored protoc");
+    std::env::set_var("PROTOC", protoc);
+
+    prost_build::compile_protos(&["resources/proto/lnrpc.proto"], &["resources/proto"])
         .expect("Failed to compile protos");
 }
